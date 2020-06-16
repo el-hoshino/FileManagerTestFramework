32a188eea1634055aa2c2d5ecfa9a11c37803079
史 翔新 <x_shi@yumemi.co.jp>
2020-06-16 20:28:54 +0900

    Add a TestObject

--- Sources/FileManagerTestFramework/FileManagerTestFramework.swift
+++ Sources/FileManagerTestFramework/FileManagerTestFramework.swift
@@ -1,3 +1,30 @@
-struct FileManagerTestFramework {
-    var text = "Hello, World!"
+import Foundation
+
+public final class TestObject {
+    
+    internal(set) public var message: String
+    
+    public init() {
+        
+        let defaultMessage = "Hello, World"
+        
+        #if DEBUG
+        // For development phase if needs to customize values frequently
+        do {
+            let fileManager = FileManager.default
+            let documentURL = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let fileURL = documentURL.appendingPathComponent("String.txt")
+            let file = try String(contentsOf: fileURL)
+            self.message = file
+        } catch {
+            debugPrint("Document URL not exist")
+            self.message = defaultMessage
+        }
+        
+        #else
+        // For production phase
+        message = defaultMessage
+        #endif
+    }
+    
 }
